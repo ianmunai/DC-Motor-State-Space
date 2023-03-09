d68495d72ecb67df00a7f4457062832ee27eb2aa
Ian Munai <imunai2002@gmail.com>
2023-03-09 08:47:34 +0300

    Updated the simulink model

--- StateSpaceRepresentation.m
+++ StateSpaceRepresentation.m
@@ -4,6 +4,7 @@ close all
 
 % -------------------STATE SPACE REP OF A DC MOTOR-----------------------
 
+syms R Kv Kt Rm La c Jm
 %define constants
 R=0.05;
 Kv=0.09854;
@@ -13,7 +14,7 @@ La=0.0015581;
 c=0.00039719;
 Jm=0.00137;
 
-%`A is an n*n matrix where n is the nuumber of states
+%`A is an n*n matrix where n is the number of states
 % B is an n*m matrix where m is the number of inputs
 % C is a p*n matrix. p is the number of outputs
 % D is a p*m matrix. m is the number of inputs
@@ -24,7 +25,7 @@ A= [0      1       0;
     0   -Kv/La  -(Rm+R)/La];
 
 B=[  0      1;
-     0     -1/Jm; 
+      0     -1/Jm; 
      1/La    0    ];
     
 C=[ 0 1 0;
@@ -34,7 +35,14 @@ C=[ 0 1 0;
 D=[ 0 0;
     0 0;
     0 0];
-[num,den]=ss2tf(A,B,C,D,1)
-% [num,den]=ss2tf(A,B(:,1),C(1,:),D(1,1));
+
+% [num,den]=ss2tf(A,B,C,D,1);
+[num,den]=ss2tf(A,B(:,1),C(1,:),D(1,1));
 %first column for B and first row for C.
-% sys=tf(num,den)
+sys=tf(num,den);
+sysmin=minreal(sys);
+
+%alternatively
+sys1=ss(A,B,C,D);
+sys2=tf(sys1)
+
